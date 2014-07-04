f0d13ca11a9ecb47267643b18d086663fed44da3
Reiter Wolfgang <wr0112358@gmail.com>
2014-07-04 20:26:28 +0200

    add missing include

--- file_util.hh
+++ file_util.hh
@@ -24,6 +24,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include <functional>
 #include <fstream>
 #include <string>
+#include <system_error>
 
 namespace libaan {
 namespace util {
